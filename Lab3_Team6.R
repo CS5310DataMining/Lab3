e577ad12f46cc057867a5a0c8abd929e0c8f63f7
RahimGit-dev <53982956+RahimGit-dev@users.noreply.github.com>
2020-02-16 23:32:03 -0600

    Final Update

--- Lab3_Team6.R
+++ Lab3_Team6.R
@@ -12,55 +12,79 @@
 #--------------------------------------------
 
 # Read CSV file
-Cust <- read.csv("Lab-3.csv", na.strings = NA)
-Cust
+cust <- read.csv("Lab-3.csv", na.strings = NA)
+cust
 
 # Add Tidyverse
 library(tidyverse)
+
+#---------------------------------------------------------
+# Step 2 Create Summary of Missing and Inconsistent Values
+#---------------------------------------------------------
+
+#Missing Values
+mv_country <- sum(is.na(cust$Country))
+mv_age <- sum(is.na(cust$Age))
+mv_salary <- sum(is.na(cust$Salary))
+mv_purchased <- sum(is.na(cust$Purchased))
+
+#Inconsistent Values
+ic_country <- 0
+ic_age <- 0
+ic_salary <- 0
+ic_purchased <- 3
+
+#Summary Table
+sum_mv_iv <- data.frame("Features" = c('Country','Age','Salary','Purchased'),
+                        "MV" = c(mv_country,mv_age,mv_salary,mv_purchased),
+                        "Percentage Of MV" = c(mv_country/10,mv_age/10,mv_salary/10,mv_purchased/10),
+                        "IV" = c(ic_country,ic_age,ic_salary,ic_purchased),
+                        "Percentage of IV" = c(ic_country/10,ic_age/10,ic_salary/10,ic_purchased/10),
+                        stringsAsFactors = FALSE)
+View(sum_mv_iv)
 
 #---------------------------------------------
 # Step 2 Organize Data
 #---------------------------------------------
 
 # Sort Data
-Cust <-Cust %>% 
+cust <-cust %>% 
   arrange(Country)
 
 # Group Data by Country & Summay (France)
-cust_fran <- lab_3[1:4,] 
+cust_fran <- cust[1:4,] 
 summary(cust_fran)
 
 # Group Data by Country & Summay (Germany)
-cust_germ <- lab_3[5:7,] 
+cust_germ <- cust[5:7,] 
 summary(cust_germ)
 
 # Group Data by Country & Summay (Spain)
-cust_spain <- lab_3[8:10,] 
+cust_spain <- cust[8:10,] 
 summary(cust_spain)
 
 # Changing NA values in Age column by computing the feature
 # mean grouped by country.
-Cust[4,2] <- round(mean(cust_fran$Age,na.rm = TRUE), digits = 1)
-Cust[10,2] <- round(mean(cust_spain$Age, na.rm=TRUE), digits = 1)
-Cust
+cust[4,2] <- round(mean(cust_fran$Age,na.rm = TRUE), digits = 1)
+cust[10,2] <- round(mean(cust_spain$Age, na.rm=TRUE), digits = 1)
+cust
 
 # Changing NA Values in Salary column by computing the
 # feature mean grouped by country
-Cust[6,3] <- round(mean(cust_germ$Salary,na.rm = TRUE))
-Cust[8,3] <- round(mean(cust_spain$Salary, na.rm=TRUE), digits = 1)
-Cust
+cust[6,3] <- round(mean(cust_germ$Salary,na.rm = TRUE))
+cust[8,3] <- round(mean(cust_spain$Salary, na.rm=TRUE), digits = 1)
+cust
 
 # Correcting the the Purchased column with the correct values
 # (Yes or No)
-Cust[4,4] <- c("Yes")
-Cust[9,4] <- c("No")
-Cust[10,4] <- c("No")
-Cust
+cust[4,4] <- c("Yes")
+cust[9,4] <- c("No")
+cust[10,4] <- c("No")
+cust
 
 #------------------------------------------------
 # Step 3 Save File
 # ----------------------------------------------
 
 #Creating table and saving as csv file
-write.csv(Cust, file = "Lab3_Team6.csv", na = " ")
-
+write.csv(cust, file = "Lab3_Team6.csv", na = " ")
